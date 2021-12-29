3115d24e6d6571a99424d8307a4ca5764a989191
XQLWL1 <vivien.sebestyen@stud.uni-corvinus.hu>
2021-12-29 17:18:23 +0100

    E-mail validálás teszt kifejtése

--- UnitTestExample/UnitTestExampleText/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExampleText/AccountControllerTestFixture.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExampleText
 {
@@ -12,7 +13,14 @@ namespace UnitTestExampleText
         [Test]
         public void TestValidateEmail(string email, bool expectedResult)
         {
+            //arrange: az első lépés mindig a teszteléshez szükséges elemek összegyűjtés és beállítása
+            var accountController = new AccountController();
 
+            //Act: a második a tesztelni kívánt tevékenység végrehajtása
+            var actualResult = accountController.ValidateEmail(email);
+
+            //Assert: a harmadik pedig az eredmények helyességének ellenőrzés
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }

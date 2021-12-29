c4c7242bee4b78dcb6490f572a8ffb41e3e7cda0
XQLWL1 <vivien.sebestyen@stud.uni-corvinus.hu>
2021-12-29 19:46:48 +0100

    Jelszó validálás teszt

--- UnitTestExample/UnitTestExampleText/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExampleText/AccountControllerTestFixture.cs
@@ -25,6 +25,33 @@ namespace UnitTestExampleText
             //Act: a második a tesztelni kívánt tevékenység végrehajtása
             var actualResult = accountController.ValidateEmail(email);
 
+            //Assert: a harmadik pedig az eredmények helyességének ellenőrzés. összehasonlítjuk az elvárt eredményt a tényleges eredményével
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        /*jelszóval kapcsolatos elvárások az accountController.cs-ben van leírva
+         * "A jelszó legalább 8 karakter hosszú kell legyen, 
+         * csak az angol ABC betűiből és számokból állhat, és 
+         * tartalmaznia kell legalább egy kisbetűt, 
+         * egy nagybetűt és egy számot.");*/
+
+        [Test,
+            TestCase("abc123",false),
+            TestCase("ABCDABCD", false),
+            TestCase("abcdabcd", false),
+            TestCase("Abc1", false),
+            TestCase("Abcabc11", true)
+            ]
+
+        public void TesztValidatePassword(string password, bool expectedResult)
+        {
+            //arrange: az első lépés mindig a teszteléshez szükséges elemek összegyűjtés és beállítása
+            var accountController = new AccountController();
+
+            //Act: a második a tesztelni kívánt tevékenység végrehajtása
+            var actualResult = accountController.ValidatePassword(password);
+
             //Assert: a harmadik pedig az eredmények helyességének ellenőrzés. összehasonlítjuk az elvárt eredményt a másik futás eredményével
             Assert.AreEqual(expectedResult, actualResult);
         }
